d50bd5a97f79ce61e5a91902bdf45bd9723737a7
Cesar Baron <cesarbaronr@gmail.com>
2025-04-01 19:33:48 -0500

    Create CesarBaron.py
    This is my first excercise

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/CesarBaron.py
@@ -0,0 +1,45 @@
+# https://www.python.org/
+
+# Comment option one 
+"""
+
+This is the option two and does't affect the code
+
+"""
+
+'''
+This is the third option and doens't affect the code
+
+'''
+
+
+"""
+How to copy a variable 
+Ctrl + D  
+
+"""
+
+
+my_firstvariable = "My variable"
+
+my_firstvariable = "second variable" 
+
+
+MY_CONSTANT = "My variable" # the programers take this like a constant
+
+"""
+Types of data
+Primitives
+"""
+
+my_int = 1 
+my_float = 1.5
+my_bool = False
+my_bool = True
+my_string = "This is my string"
+my_other_string= 'This is another string'
+
+
+print("This is my first code in phyton")
+
+print(type(my_string))  # with type we can verify the type of data 
